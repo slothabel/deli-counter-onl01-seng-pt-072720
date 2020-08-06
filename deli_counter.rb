9af128f12b14cf7ea0e7e64de48f53dc1d5f669f
Yoshi Mejia <yoshimejia00@gmail.com>
2020-08-06 18:12:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+katz_deli = []
+
+def line(place)
+  counter = 0
+  if counter == 0
+    puts "The line is currently empty."
+  elsif counter > 1
+    puts "The line is currently: #{count+1} #{place}"
+  end
+end
